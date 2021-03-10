5ebe1b7ec4a2b56d9692ce87980251d672d8cda9
OdedHarShefer <odedharshefer@gmail.com>
2021-03-10 19:41:15 +0200

    Initial commit

---
+++ app/src/main/java/com/example/mydbcontactapp/DisplayAllContacts.java
@@ -0,0 +1,27 @@
+package com.example.mydbcontactapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class DisplayAllContacts extends AppCompatActivity {
+
+    ListView lvContacts;
+    ArrayList<Contact> ary;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_all_contacts);
+
+        lvContacts = findViewById(R.id.listViewContacts);
+        Dal dal = new Dal(this);
+        ary = dal.getAllContacts();
+
+        ContactAdapter ad = new ContactAdapter(this, R.layout.contact_layout, ary);
+        lvContacts.setAdapter(ad);
+    }
+}
